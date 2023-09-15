3f8ae0dec11b8cac0f90ee7dea455822cf3e484c
ljaycaliwan <caliwanlj27@gmail.com>
2023-09-15 22:22:54 +0800

    added CD workflow and a demo controller

---
+++ cm-backend/src/main/java/DemoController.java
@@ -0,0 +1,12 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    record DemoMessage(String message){}
+
+    @GetMapping("/demo")
+    public DemoMessage demoMessage(){
+        return new DemoMessage("Test Continuous Integration is Working.");
+    }
+}
