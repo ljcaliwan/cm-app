6b5a0aef85729790cc8288d9088ce651bd5d9585
ljaycaliwan <caliwanlj27@gmail.com>
2023-09-16 16:22:55 +0800

    remove record

--- cm-backend/src/main/java/dev/ljcaliwan/cmbackend/DemoController.java
+++ cm-backend/src/main/java/dev/ljcaliwan/cmbackend/DemoController.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private static int COUNTER = 0;
-    record DemoMessage(String message){}
-
     @GetMapping("/demo")
-    public DemoMessage demoMessage(){
-        return new DemoMessage(("" +
-                "Test Continuous Integration is Working. The value of counter is: ").formatted(++COUNTER)
-        );
+    public String demoMessage(){
+        return ("Test Continuous Integration is Working. The value of counter is: ").formatted(++COUNTER);
     }
 }
